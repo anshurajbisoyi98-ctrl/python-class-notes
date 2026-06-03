f8e33b13aa6143dece869907322a05e5e1b8443d
anshurajbisoyi98-ctrl <anshurajbisoyi98@gmail.com>
2026-06-03 09:35:29 +0530

    Add files via upload

---
+++ hii.py
@@ -0,0 +1,42 @@
+for i in range(51):
+  if(i%2==0):
+    print(i)
+for i in range(51):
+  if(i%2!=0):
+    print(i)
+a=3
+
+if a%2 == 0:
+  print("even")
+else:
+  print("odd")
+
+
+if a>0 :
+  print("Positive")
+
+elif a==0 :
+    print("neutral")
+else :
+  print("negative")
+
+Day = input("Enter Short Form: ")
+
+if Day =='Mon':
+  print("Monday")
+elif Day == 'Tue':
+  print("Tuesday")
+elif Day == 'Wed':
+  print("Wednesday")
+elif Day == 'Thr':
+  print("Thrusday")
+elif Day == 'Fri':
+  print("Friday")
+elif Day == 'Sat':
+  print("Saturday")
+else :
+  print("Sunday")
+
+while a<10:
+  print(a)
+  a=a+1
